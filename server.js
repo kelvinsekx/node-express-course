c7807006da22de4089a83005e497bc24079a6ef2
kelvinsekx <utdkelvin@gmail.com>
2020-04-25 14:01:22 -1200

    added first GET routegit add server.jsOOpps, my second

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const PORT = 3000;
 
 const app = express()
 
-app.get("/", (req, res)=>{
-    res.send("yaya!")
+//USE MOCK DATA
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+
+app.get("/users", (req, res)=>{
+    res.json({
+        success: true,
+        message:'successfully got users. Nice',
+        users: mockUserData
+    })
 })
 
 app.listen(PORT, ()=>console.log(`started server on localhost:${PORT}`))
